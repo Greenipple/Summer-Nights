266bb2f5083a81297364feae80a0368b9e815e60
Code Cadet <codecadet@cobol.local>
2020-08-26 20:55:53 +0100

    Add webcontroller

--- src/main/java/lasanha/summertime/controller/web/MainController.java
+++ src/main/java/lasanha/summertime/controller/web/MainController.java
@@ -1,15 +1,24 @@
 package lasanha.summertime.controller.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
     @RequestMapping(path = {"/home", "/", ""})
-    public String home() {
+    public String showHome() {
         return "home";
     }
+
+    @RequestMapping(path = "/tellmemore/{id}")
+    public String showTellMeMore(@PathVariable Integer id) {
+
+        return "tellmemore";
+    }
+
+
 
 
 

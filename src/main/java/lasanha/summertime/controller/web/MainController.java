fbfbb55f2c2c9ffb9fb9ea846607684187d4e193
Code Cadet <codecadet@cobol.local>
2020-08-26 16:57:16 +0100

    hello world??

--- src/main/java/lasanha/summertime/controller/web/MainController.java
+++ src/main/java/lasanha/summertime/controller/web/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-    @RequestMapping("/")
+    @RequestMapping(path = {"/home", "/", ""})
     public String home() {
         return "user/home";
     }

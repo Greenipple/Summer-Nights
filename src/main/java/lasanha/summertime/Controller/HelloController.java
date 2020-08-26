3679bfcd4a895ee798f74d48552728906787d27c
Code Cadet <codecadet@simula.local>
2020-08-26 13:37:07 +0100

    first commit

---
+++ src/main/java/lasanha/summertime/Controller/HelloController.java
@@ -0,0 +1,16 @@
+package lasanha.summertime.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String sayHi() {
+        return "<h1>hi</h1>";
+    }
+
+
+
+}
